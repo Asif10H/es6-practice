ba2225aabd4db67f8b449bbc4ed371cd7338e11e
Asif10H <habibullahasif71@gmail.com>
2021-02-03 12:42:54 +0600

    let and const

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+// numbers = ["Sunny", "Zia", "Hossain"]
+console.log(numbers);
+
+const nayok = {name: "sakib khan", phone: 44444};
+console.log(nayok);
+
+let patientName = "Hossain";
+patientName = "Zia";
+console.log(patientName);
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum += i;
+}
+console.log(i);
